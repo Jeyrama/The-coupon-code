ffbebd6c80ddff9a9a3f02a69d6c6befbc6c5043
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-24 19:33:57 -0700

    Update The-coupon-code.js

--- The-coupon-code.js
+++ The-coupon-code.js
@@ -16,3 +16,7 @@ Examples:
 
 
 // Solution
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}
